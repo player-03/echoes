89546a0048cbe34914d44dd5b6c2dc6f9e8f997a
Joseph Cloutier <jkcloutier@gmail.com>
2022-07-10 13:30:52 -0400

    Make read-only types `Iterable`.

--- src/echoes/utils/ReadOnlyData.hx
+++ src/echoes/utils/ReadOnlyData.hx
@@ -2,13 +2,13 @@ package echoes.utils;
 
 @:forward(first, iterator, isEmpty, last, length)
 @:forward.new
-abstract ReadOnlyList<T>(List<T>) from List<T> {
+abstract ReadOnlyList<T>(List<T>) from List<T> to Iterable<T> {
 	public inline function has(item:T):Bool return Lambda.has(this, item);
 }
 
 @:forward(contains, iterator, length)
 @:forward.new
-abstract ReadOnlyArray<T>(Array<T>) from Array<T> {
+abstract ReadOnlyArray<T>(Array<T>) from Array<T> to Iterable<T> {
 	@:arrayAccess private inline function get(index:Int):T {
 		return this[index];
 	}
