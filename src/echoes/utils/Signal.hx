1dafae4b85d1324579f5335f446332b01e58726a
Joseph Cloutier <jkcloutier@gmail.com>
2022-07-10 17:22:37 -0400

    Fix `Signal` method comparison.
    Normal equality testing doesn't work for instance functions. This may
    have been overlooked in the past due to the use of anonymous functions,
    but going forward it needs to work for everything.

--- src/echoes/utils/Signal.hx
+++ src/echoes/utils/Signal.hx
@@ -11,4 +11,25 @@ abstract Signal<T:haxe.Constraints.Function>(Array<T>) {
 			listener($a{args});
 		};
 	}
+	
+	public function contains(listener:T):Bool {
+		for(l in this) {
+			if(Reflect.compareMethods(l, listener)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public function remove(listener:T):Bool {
+		for(i => l in this) {
+			if(Reflect.compareMethods(l, listener)) {
+				this.splice(i, 1);
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
