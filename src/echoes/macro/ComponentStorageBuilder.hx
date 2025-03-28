c1806158f3fc53af065c4468013e78e469ed8635
Joseph Cloutier <player3.14@gmail.com>
2025-03-28 02:00:02 -0400

    Don't generate component storage singletons in display/completion mode.

--- src/echoes/macro/ComponentStorageBuilder.hx
+++ src/echoes/macro/ComponentStorageBuilder.hx
@@ -20,6 +20,10 @@ class ComponentStorageBuilder {
 	private static var registered:Bool = false;
 	
 	public static inline function getComponentStorage(componentComplexType:ComplexType):Expr {
+		if(Context.defined("display") || Sys.args().indexOf("--no-output") >= 0) {
+			return macro new echoes.ComponentStorage<$componentComplexType>("For code completion only. If you see this at runtime, it's an error.");
+		}
+		
 		return macro @:pos(Context.currentPos()) $i{ getComponentStorageName(componentComplexType) }.instance;
 	}
 	
