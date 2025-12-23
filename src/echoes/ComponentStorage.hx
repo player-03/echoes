player-03/echoes

commit 99aa911e64e62598d4a6a8eb2897a0f65e72a2b0

Implement `ComponentStorage.addDynamic()`.
With an option to opt out, in case greater type safety is desired.

src/echoes/ComponentStorage.hx
@@ -9,6 +9,7 @@ import echoes.View;
 import haxe.Exception;
 import haxe.Serializer;
 import haxe.Unserializer;
+import Type;
 
 /**
  * A central location to store all components of a given type. For example, the
@@ -89,8 +90,11 @@ class ComponentStorage<T> {
 	private final storage:Array<Null<T>> = [];
 	#end
 	
-	public inline function new(componentType:String) {
+	private final valueType:ValueType;
+	
+	public inline function new(componentType:String, ?valueType:ValueType) {
 		this.componentType = componentType;
+		this.valueType = valueType != null ? valueType : TUnknown;
 		Echoes._componentStorage.push(this);
 		
 		//Some platforms get confused by the declaration of `Array<Null<T>>`,
@@ -152,6 +156,26 @@ class ComponentStorage<T> {
 		}
 	}
 	
+	#if !echoes_no_addDynamic
+	
+	/**
+	 * Adds the given component if it's the correct type. Returns whether the
+	 * component was successfully added.
+	 * 
+	 * Caution: this only checks type information that's available at runtime,
+	 * which means it can't verify type parameters.
+	 */
+	public function addDynamic(entity:Entity, component:Dynamic):Bool {
+		if(isCorrectType(component)) {
+			add(entity, cast component);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	#end
+	
 	@:allow(echoes.Echoes)
 	private inline function clear():Void {
 		#if (echoes_storage == "Map")
@@ -179,6 +203,42 @@ class ComponentStorage<T> {
 	
 	public inline function get(entity:Entity):Null<T> {
 		return storage[entity.id];
+	}
+	
+	/**
+	 * Checks whether the given component can be added to this storage.
+	 * 
+	 * Due to limitations of Haxe's type system, this can only verify basic
+	 * types, class instances, and enum values. It will always return false for
+	 * functions and anonymous structures.
+	 * 
+	 * Also, type parameters cannot be checked at runtime, so (for instance)
+	 * this will treat `Array<Int>` and `Array<String>` as the same.
+	 */
+	public function isCorrectType(component:Dynamic):Bool {
+		switch(valueType) {
+			case TNull:
+				//Special case: `TNull` means this was created as a
+				//`DynamicComponentStorage` and should accept all components.
+				return true;
+			case TBool:
+				return Std.isOfType(component, Bool);
+			case TInt:
+				return Std.isOfType(component, Int);
+			case TFloat:
+				return Std.isOfType(component, Float);
+			case TClass(c):
+				return c == Type.getClass(component);
+			case TEnum(e):
+				switch(Type.typeof(component)) {
+					case TEnum(e2):
+						return e == e2;
+					default:
+						return false;
+				}
+			default:
+				return false;
+		}
 	}
 	
 	public function remove(entity:Entity):Void {
@@ -311,18 +371,18 @@ class ComponentStorage<T> {
 
 /**
  * A version of `ComponentStorage` that stores components of unknown type.
- * `add()` is disabled because there's no way to make sure the added component
- * is the correct type. `remove()` is still available because it doesn't need to
- * check any types.
- * 
- * If you're creating the `ComponentStorage` at runtime and want to be able to
- * add components, use `new ComponentStorage<Dynamic>()` instead. Obviously, no
- * type checking will be performed.
+ * Since this strips the compile-time type checks, you must use `addDynamic()`
+ * instead of `add()`.
  */
-@:forward(clear, componentType, exists, get, name, relatedViews, remove, removeAll, shortComponentType)
+@:forward(clear, componentType, exists, get, isCorrectType, name, relatedViews, remove, removeAll, shortComponentType #if !echoes_no_addDynamic , addDynamic #end)
 abstract DynamicComponentStorage(ComponentStorage<Dynamic>) {
 	@:from private static inline function fromComponentStorage<T>(componentStorage:ComponentStorage<T>):DynamicComponentStorage {
 		return cast componentStorage;
+	}
+	
+	public inline function new(componentType:String, ?valueType:ValueType) {
+		this = new ComponentStorage<Dynamic>(componentType,
+			valueType != null ? valueType : TNull);
 	}
 	
 	@:allow(echoes.ViewBase)

src/echoes/macro/ComponentStorageBuilder.hx
@@ -40,9 +40,36 @@ class ComponentStorageBuilder {
 			return storageTypeName;
 		}
 		
+		final valueType:Expr = switch(componentComplexType.toType().followWithAbstracts()) {
+			case TInst(_.get() => classType, _):
+				final parts:Array<String> = classType.pack.copy();
+				parts.push(classType.module);
+				if(classType.name != classType.module) {
+					parts.push(classType.name);
+				}
+				macro TClass($p{ parts });
+			case TEnum(_.get() => enumType, _):
+				final parts:Array<String> = enumType.pack.copy();
+				parts.push(enumType.module);
+				if(enumType.name != enumType.module) {
+					parts.push(enumType.name);
+				}
+				macro TEnum($p{ parts });
+			case TFun(_, _):
+				macro TFunction;
+			case TAnonymous(_):
+				macro TObject;
+			case TAbstract(_.get() => { pack: [], module: "StdTypes", name: name }, _)
+				if(name == "Float" || name == "Int" || name == "Bool"):
+				final name:String = "T" + name;
+				macro $i{ name };
+			default:
+				macro TUnknown;
+		};
+		
 		final componentTypeName:String = new Printer().printComplexType(componentComplexType);
 		final storageTypePath:TypePath = { pack: [], name: storageTypeName };
-		var getInstance:Expr = macro new echoes.ComponentStorage<$componentComplexType>($v{ componentTypeName });
+		var getInstance:Expr = macro new echoes.ComponentStorage<$componentComplexType>($v{ componentTypeName }, $valueType);
 		
 		//If a custom singleton is defined, use that instead.
 		final componentBaseType:BaseType = componentComplexType.toType().toBaseType();

src/echoes/View.hx
@@ -117,16 +117,15 @@ class ViewBase {
  * Sample usage:
  * 
  * ```haxe
- * //Storage for a custom component type. Because `entity.add(x)` only works at
- * //compile time, you'll have to call `customComponent.add(entity, x)`.
- * public final customComponent:ComponentStorage<Any>;
+ * //Storage for a custom component type.
+ * public final customComponent:DynamicComponentStorage;
  * 
  * //A view of `customComponent` and `String`; it'll dispatch events for any
  * //entity that has both components.
  * public final view:DynamicView;
  * 
  * public function new() {
- *     customComponent = new ComponentStorage<Any>("CustomComponent");
+ *     customComponent = new DynamicComponentStorage("CustomComponent");
  *     
  *     view = new DynamicView(customComponent, Echoes.getComponentStorage(String));
  *     

test/AdvancedFunctionalityTest.hx
@@ -56,9 +56,50 @@ class AdvancedFunctionalityTest extends Test {
 		Assert.isFalse(Echoes.getComponentStorage(EagerIntArray) is IntArrayStorage);
 	}
 	
+	#if !echoes_no_addDynamic
+	
+	private function testDynamicComponents():Void {
+		final entity:Entity = new Entity();
+		
+		final string:ComponentStorage<String> = Echoes.getComponentStorage(String);
+		Assert.isFalse(string.addDynamic(entity, 5));
+		Assert.isFalse(string.addDynamic(entity, ["string"]));
+		Assert.isFalse(string.exists(entity));
+		
+		Assert.isTrue(string.addDynamic(entity, "string"));
+		Assert.isTrue(string.exists(entity));
+		
+		final array:ComponentStorage<Array<String>> = Echoes.getComponentStorage((_:Array<String>));
+		Assert.isFalse(array.addDynamic(entity, 5));
+		Assert.isFalse(array.addDynamic(entity, "string"));
+		Assert.isFalse(array.exists(entity));
+		
+		Assert.isTrue(array.addDynamic(entity, ["string"]));
+		Assert.isTrue(array.exists(entity));
+		
+		Assert.isTrue(array.addDynamic(entity, [1, 2]),
+			"Int array was (correctly but unexpectedly) rejected. If Haxe can now distinguish array types, please update the tests and documentation.");
+		
+		final func:ComponentStorage<() -> Void> = Echoes.getComponentStorage((_:() -> Void));
+		Assert.isFalse(func.addDynamic(entity, (x:Int) -> x + 1),
+			"Function was accepted despite having the wrong signature.");
+		Assert.isFalse(func.addDynamic(entity, () -> trace("Hello, world")),
+			"Function with the correct signature was unexpectedly accepted. If functions can now be verified, please update the tests and documentation.");
+		
+		final dyn:DynamicComponentStorage = new DynamicComponentStorage("component");
+		Assert.isTrue(dyn.addDynamic(entity, 5));
+		Assert.isTrue(dyn.addDynamic(entity, []));
+		Assert.isTrue(dyn.addDynamic(entity, () -> ""));
+		
+		final bool:DynamicComponentStorage = new DynamicComponentStorage("bool", TBool);
+		Assert.isFalse(bool.addDynamic(entity, 5));
+		Assert.isFalse(bool.addDynamic(entity, []));
+		Assert.isTrue(bool.addDynamic(entity, false));
+	}
+	
 	private function testDynamicViews():Void {
-		final component0:ComponentStorage<Any> = new ComponentStorage<Any>("component0");
-		final component1:ComponentStorage<Any> = new ComponentStorage<Any>("component1");
+		final component0:DynamicComponentStorage = new DynamicComponentStorage("component0");
+		final component1:DynamicComponentStorage = new DynamicComponentStorage("component1");
 		
 		final view:DynamicView = new DynamicView(component0, component1);
 		view.activate();
@@ -68,20 +109,20 @@ class AdvancedFunctionalityTest extends Test {
 		view.onRemoved.add((entity, components) -> removed += components.join(""));
 		
 		final entity0:Entity = new Entity();
-		component0.add(entity0, "---");
+		component0.addDynamic(entity0, "---");
 		component0.remove(entity0);
 		Assert.equals("", added);
 		Assert.equals("", removed);
 		
-		component1.add(entity0, "b");
-		component0.add(entity0, "a");
+		component1.addDynamic(entity0, "b");
+		component0.addDynamic(entity0, "a");
 		Assert.equals("ab", added);
 		Assert.equals("", removed);
 		
 		final entity1:Entity = new Entity();
 		entity1.add("string");
-		component0.add(entity1, 0);
-		component1.add(entity1, 1);
+		component0.addDynamic(entity1, 0);
+		component1.addDynamic(entity1, 1);
 		Assert.equals("ab01", added);
 		Assert.equals("", removed);
 		
@@ -94,6 +135,8 @@ class AdvancedFunctionalityTest extends Test {
 		Assert.equals("ab01", added);
 		Assert.equals("01ab", removed);
 	}
+	
+	#end
 	
 	private function testEntityTemplates():Void {
 		new NameSystem().activate();

commit 15e70cbcc8a58e0775486e1ef3d4b4d4b3d68573

Fix compilation errors introduced in 99aa911e64e62598d4a6a8eb2897a0f65e72a2b0.

src/echoes/ComponentStorage.hx
@@ -210,7 +210,7 @@ class ComponentStorage<T> {
 	 * 
 	 * Due to limitations of Haxe's type system, this can only verify basic
 	 * types, class instances, and enum values. It will always return false for
-	 * functions and anonymous structures.
+	 * functions, anonymous structures, and private types.
 	 * 
 	 * Also, type parameters cannot be checked at runtime, so (for instance)
 	 * this will treat `Array<Int>` and `Array<String>` as the same.

src/echoes/macro/ComponentStorageBuilder.hx
@@ -41,20 +41,10 @@ class ComponentStorageBuilder {
 		}
 		
 		final valueType:Expr = switch(componentComplexType.toType().followWithAbstracts()) {
-			case TInst(_.get() => classType, _):
-				final parts:Array<String> = classType.pack.copy();
-				parts.push(classType.module);
-				if(classType.name != classType.module) {
-					parts.push(classType.name);
-				}
-				macro TClass($p{ parts });
-			case TEnum(_.get() => enumType, _):
-				final parts:Array<String> = enumType.pack.copy();
-				parts.push(enumType.module);
-				if(enumType.name != enumType.module) {
-					parts.push(enumType.name);
-				}
-				macro TEnum($p{ parts });
+			case TInst(_.get() => classType, _) if(!classType.isPrivate):
+				macro TClass(${ classType.toClassExpr() });
+			case TEnum(_.get() => enumType, _) if(!enumType.isPrivate):
+				macro TEnum(${ enumType.toClassExpr() });
 			case TFun(_, _):
 				macro TFunction;
 			case TAnonymous(_):

src/echoes/macro/MacroTools.hx
@@ -10,6 +10,7 @@ import haxe.macro.Type;
 
 using haxe.macro.ComplexTypeTools;
 using haxe.macro.Context;
+using haxe.macro.MacroStringTools;
 using Lambda;
 
 class MacroTools {
@@ -257,6 +258,17 @@ class MacroTools {
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * Converts a type to a `Class<T>` or `Enum<T>` expression.
+	 */
+	public static inline function toClassExpr(type:BaseType, ?pos:Position):Expr {
+		final parts:Array<String> = type.module.split(".");
+		if(type.name != parts[parts.length - 1]) {
+			parts.push(type.name);
+		}
+		return parts.toFieldExpr(pos);
 	}
 	
 	/**