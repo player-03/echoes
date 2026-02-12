970a8bdbafb4739ce788720e0e10af51abf34672
Joseph Cloutier <player3.14@gmail.com>
2026-02-12 00:35:32 -0500

    Revert "Add `UTest.runCases()`, for easily running tests in bulk."
    This reverts commit b3b4c84caa601e61b8e26d1ec36a2ffb7a131b58.

--- src/echoes/test/UTest.hx
+++ src/echoes/test/UTest.hx
@@ -2,7 +2,6 @@ package echoes.test;
 
 #if utest
 
-import haxe.macro.Expr;
 import utest.ITest;
 import utest.Runner;
 
@@ -51,29 +50,6 @@ class UTest {
 		
 		utest.ui.Report.create(runner);
 		runner.run();
-	}
-	
-	/**
-	 * Runs all test cases from the given packages, including sub-packages,
-	 * calling `Echoes.reset()` after each one.
-	 * @param packages One or more dot paths as a string or field expression.
-	 * Pass multiple arguments if needed, not an array.
-	 * @see utest.Runner#addCases
-	 */
-	public static macro function runCases(packages:Array<Expr>):Expr {
-		final cases:Array<Expr> = [for(p in packages)
-			macro runner.addCases($p)];
-		
-		return macro {
-			var runner = new utest.Runner();
-			
-			$b{ cases }
-			
-			echoes.test.UTest.resetBetweenTests(runner);
-			
-			utest.ui.Report.create(runner);
-			runner.run();
-		};
 	}
 }
 
